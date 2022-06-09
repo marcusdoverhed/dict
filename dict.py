d2b104af48cb221b1b24b2a0501ae677bfa1607c
marcusdoverhed <marcusdov@hotmail.com>
2022-06-09 16:27:49 +0200

    fixing an issue

--- dict.py
+++ dict.py
@@ -43,23 +43,29 @@ commands = [
 
 
 for x in commands:
-
     print(x)
-while True: ## REPL - Read Execute Program Loop
-    cmd = input("Command: ")
-    if cmd == "list":
-        print(" Here is the list")
-        print(read_dict(conn))
-    elif cmd == "add":
-        name = input("  Word: ")
-        phone = input("  Translation: ")
-        add_word(conn, name, phone)
-        print(f" Added word {name}")
-    elif cmd == "delete":
-        ID = input("  ID: ")
-        delete_word(conn, ID)
-        print(f" deleted word {ID}")
-    elif cmd == "quit":
-        save_dict(conn)
-        print(" GOODBYE ")
-        exit()
+    
+
+
+
+def main():
+        
+     while True: ## REPL - Read Execute Program Loop
+        cmd = input("Command: ")
+        if cmd == "list":
+            print(" Here is the list")
+            print(read_dict(conn))
+        elif cmd == "add":
+            name = input("  Word: ")
+            phone = input("  Translation: ")
+            add_word(conn, name, phone)
+            print(f" Added word {name}")
+        elif cmd == "delete":
+            ID = input("  ID: ")
+            delete_word(conn, ID)
+            print(f" deleted word {ID}")
+        elif cmd == "quit":
+            save_dict(conn)
+            print(" GOODBYE ")
+            exit()
+main()
